5ee612fd1a3253472e327769cd4aad371383424c
Ayaanshaikh20 <email@youremail.com>
2023-06-16 13:23:18 +0530

    3

--- digital clock.js
+++ digital clock.js
@@ -6,16 +6,14 @@ let ticking = tickClock = () => {
     let currentDate = new Date()
 
     let getHr = currentDate.getHours();
-
     let getMin = currentDate.getMinutes();
-
     let getsec = currentDate.getSeconds();
     
     digitalHour.textContent = getHr;
 
     digitalminute.textContent = getMin;
 
-    digitalsecond.textContent = getsec
+    digitalsecond.textContent = getsec;
 }
 
 setInterval(ticking, 1000)
