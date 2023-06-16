a2cb3b461a5358f94d6d06653a306e1c8ee5a659
Ayaanshaikh20 <email@youremail.com>
2023-06-16 18:16:49 +0530

    16 june

---
+++ OOP concept.js
@@ -0,0 +1,13 @@
+//Object oriented programming
+
+//Constrctor function and the "new" operator
+
+//We can not use arrow function as constructor
+
+//Call "constructor" function using "new" keyword
+
+//1 new {object} created empty initially
+//2 "this" = {object}
+//3 object linked to prototype
+//4 functional automatically return {object}
+
